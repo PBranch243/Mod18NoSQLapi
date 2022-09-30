e728be94bba5b9b547ecb5c5ee055e1ccf538e23
unknown <philbranch243@gmail.com>
2022-09-30 15:48:19 -0500

    method to remove/delete a thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -13,7 +13,7 @@ const thoughtController = {
                 );
             })
             .then(dbUserData => {
-                if(!dbUserData) {
+                if (!dbUserData) {
                     res.status(404).json({ message: 'No user found with this id' });
                     return;
                 }
@@ -23,10 +23,27 @@ const thoughtController = {
     },
 
     //Delete Thought
-    removeThought() {
-
-
+    removeThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+            .then(deletedThought => {
+                if (!deletedThought) {
+                    return res.status(404).json({ message: 'No thought with this id!' });
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull: { thoughts: params.thoughtId } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
     }
-};
+}
 
 module.exports = thoughtController;
