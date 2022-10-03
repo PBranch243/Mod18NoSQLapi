4c143ceabf0b473af0897f9dd7ed3e6c53d44634
unknown <philbranch243@gmail.com>
2022-10-03 09:35:49 -0500

    added reaction routes, now to test

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -53,6 +53,32 @@ const thoughtController = {
                 }
                 res.json(updatedThought);
             })
+    },
+    //Add a reaction
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
+    },
+    //delete a reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { replies: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.json(err));
     }
 }
 
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { removeThought, addThought, updateThought } = require('../../controllers/thought-controller');
+const { removeThought, addThought, updateThought, addReaction, removeReaction } = require('../../controllers/thought-controller');
 
 // /api/thoughts/<userId>
 router.route('/:userId').post(addThought);
@@ -7,7 +7,13 @@ router.route('/:userId').post(addThought);
 // /api/thoughts/<userId>/<thoughtId>
 router.route('/:userId/:thoughtId').delete(removeThought);
 
-// /api/thoughts/<userId>/thoughtId>
+// /api/thoughts/<userId>/<thoughtId>
 router.route('/:userId/:thoughtId').put(updateThought);
+
+// /api/thoughts/<userId>/<thoughtId>/<reactionId>
+router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
+
+// /api/thoughts/<userId>/<thoughtId>/<reactionId>
+router.route('/:userId/:thoughtId/:reactionId').put(addReaction);
 
 module.exports = router;
